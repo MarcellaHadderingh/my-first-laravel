6705e1e47f301e01a5d519eb04bba3c7118152ac
marcella <marcellavanduyn@gmail.com>
2019-01-24 11:55:37 +0100

    test

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+   
+<title>@yield('title')</title>
+
+</head>
+
+<body>
+  
+
+
+    <ul>
+       <li><a href="/">Home</a></li>
+       <li><a href="/about">About</a></li>
+       <li><a href="/contact">Contact</a></li>
+</ul>
+@yield('content')
+
+</body>
+
+</html>
