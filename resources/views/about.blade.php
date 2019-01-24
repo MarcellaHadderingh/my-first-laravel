67700e820788d1000812cd69a021f20fb93a9d79
marcella <marcellavanduyn@gmail.com>
2019-01-24 14:06:01 +0100

    lorem tekst
    lorem tekst

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,5 +5,8 @@
 
 @section('content')
     <h1>About us</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Provident eius sequi dolore laborum accusantium repellat doloremque tempora repudiandae architecto consectetur asperiores, 
+        est et cum debitis impedit ipsum nihil quasi tempore.</p>
 
 @endsection
